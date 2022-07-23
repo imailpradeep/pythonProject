eec3f5d7209e46a0e2111c6059900a2d764c1c40
imailpradeep <imailpradeep1@gmail.com>
2022-07-23 13:39:55 +0530

    test

---
+++ DataTest.py
@@ -0,0 +1,15 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://imailpradeep:ammaacha@cluster0.gujy4jv.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name":"Pradeep",
+    "email" : "imailpradeep1@gmail.com",
+    "surname" : "Bhaskar"
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
